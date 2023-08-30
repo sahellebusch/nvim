8beea137aeefec4894376b7733c69ddb67b60296
Sean Hellebusch <sean.hellebusch@hingehealth.com>
2023-08-30 09:28:48 -0600

    feat: use everforest, telecope find in buff map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,6 +17,14 @@ vim.keymap.set("i", "<C-b>", "<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-e>", "<End>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-a>", "<Home>", { noremap = true, silent = true })
 
+-- Insert Mode Bindings for Ctrl + n and Ctrl + p
+vim.keymap.set("i", "<C-n>", "<Down>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-p>", "<Up>", { noremap = true, silent = true })
+
+-- Visual Mode Bindings for Ctrl + n and Ctrl + p
+vim.keymap.set("v", "<C-n>", "j", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-p>", "k", { noremap = true, silent = true })
+
 -- Visual mode bindings
 vim.keymap.set("v", "<C-f>", "<Right>", { noremap = true, silent = true })
 vim.keymap.set("v", "<C-b>", "<Left>", { noremap = true, silent = true })
@@ -29,3 +37,11 @@ vim.keymap.set("n", "<M-a>", "ggVG", { noremap = true, silent = true })
 -- Get back my old emacs deleteion behavior
 vim.keymap.set("n", "<C-d>", "x", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-d>", "<Del>", { noremap = true, silent = true })
+
+-- Telescope
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>fi",
+  ":Telescope current_buffer_fuzzy_find<CR>",
+  { noremap = true, silent = true }
+)
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "nordtheme/vim", name = "nord" },
+  -- { "folke/tokyonight.nvim" },
+  { "sainnhe/everforest", name = "everforest" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+      background = "light",
+    },
+  },
+}
